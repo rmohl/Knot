4b6ac068190b2da989fa60dd1c52c259f4193cb8
rmohl <mohlrachael@gmail.com>
2024-03-12 21:39:08 -0400

    added assets, worked on instructions page

--- lib/InstructionsPage.dart
+++ lib/InstructionsPage.dart
@@ -2,19 +2,31 @@ import 'package:flutter/material.dart';
 
 class InstructionsPage extends StatelessWidget {
   final ThemeData theme;
-  final SizedBox space = const SizedBox(height: 10);
+  final SizedBox vSpace = const SizedBox(height: 10);
+  final SizedBox hSpace = const SizedBox(width: 10);
 
   InstructionsPage({super.key, required this.theme});
 
-  Column subtitleText(text){
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          space,
-          Text(text, style: theme.textTheme.titleMedium),
-          space,
-        ]
-    );
+  Column subtitleText(text, {startingSpace = true}){
+    if (startingSpace) {
+      return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            vSpace,
+            Text(text, style: theme.textTheme.titleMedium),
+            vSpace,
+          ]
+      );
+    } else {
+      return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(text, style: theme.textTheme.titleMedium),
+            vSpace,
+          ]
+      );
+    }
+
   }
 
   @override
@@ -22,79 +34,198 @@ class InstructionsPage extends StatelessWidget {
     return SingleChildScrollView(
         child: Column(
             children: <Widget>[
-              Card(
-                child: ListTile(
-                  tileColor: theme.primaryColorLight,
+              Card (
+                child: ExpansionTile(
                   title: Text(
                       'How to Create a String Bracelet',
                       style: theme.textTheme.headlineSmall
                   ),
-                ),
+                  children: <Widget>[
+                    ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            subtitleText("1. Gather Your Materials", startingSpace: false),
+                            Text(" • Embroidery floss or string", style: theme.textTheme.bodyMedium),
+                            Text(" • Tape or clipboard (for holding the string in place!)", style: theme.textTheme.bodyMedium),
+                            subtitleText("2. Pick Your Pattern"),
+                            Text("A pattern will allow you to determine how many different strings you need, how many different colours will be in your bracelet, and what knots to tie in order to create your bracelet.", style: theme.textTheme.bodyMedium),
+                            subtitleText("3. Setting Up Your Bracelet"),
+                            Text("Tie your strings together ", style: theme.textTheme.bodyMedium),
+                            subtitleText("4. Creating Your Bracelet"),
+                            subtitleText("5. Finishing Your Bracelet"),
+                            vSpace
+                          ],
+                        )
+                    ),
+                  ],
+                )
               ),
-              Card(
-                child: ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        subtitleText("1. Gather Your Materials"),
-                        Text(" • Embroidery floss or string", style: theme.textTheme.bodyMedium),
-                        Text(" • Tape or clipboard (for holding the string in place!)", style: theme.textTheme.bodyMedium),
-                        subtitleText("2. Pick Your Pattern"),
-                        Text("A pattern will allow you to determine how many different strings you need, how many different colours will be in your bracelet, and what knots to tie in order to create your bracelet.", style: theme.textTheme.bodyMedium),
-                        subtitleText("3. Setting Up Your Bracelet"),
-                        subtitleText("4. Creating Your Bracelet"),
-                        subtitleText("5. Finishing Your Bracelet"),
-                        space
-                      ],
-                    )
-                ),
+              Card (
+                  child: ExpansionTile(
+                    title: Text(
+                        'Types of Knots',
+                        style: theme.textTheme.headlineSmall
+                    ),
+                    children: <Widget>[
+                      ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              subtitleText("Forward Knot", startingSpace: false),
+                              Text("1. Create a '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/forward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              vSpace,
+                              Text("2. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/forward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/forward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                              Text("3. Repeat once more!", style: theme.textTheme.bodyMedium),
+                              subtitleText("Backward Knot"),
+                              Text("1. Create a backwards '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/backward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              vSpace,
+                              Text("2. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/backward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/backward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                              Text("3. Repeat once more!", style: theme.textTheme.bodyMedium),
+                              subtitleText("Forward-Backward Knot"),
+                              Text("1. Create a '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/forward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              vSpace,
+                              Text("2. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/forward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/forward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                              Text("3. Create a backwards '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/forward-backward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              vSpace,
+                              Text("4. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/forward-backward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/forward-backward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                              subtitleText("Backward-Forward Knot"),
+                              Text("1. Create a backwards '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/backward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              vSpace,
+                              Text("2. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/backward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/backward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                              Text("3. Create a '4' by placing your working string over your interior string.", style: theme.textTheme.bodyMedium),
+                              Container(
+                                width: double.infinity,
+                                child: Image.asset("assets/backward-forward1.jpeg", fit: BoxFit.fitWidth),
+                              ),
+                              Text("4. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
+                              vSpace,
+                              Container(
+                                  width: double.infinity,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Image.asset("assets/backward-forward2.jpeg", scale: 4.75),
+                                      hSpace,
+                                      Image.asset("assets/backward-forward3.jpeg", scale: 4.75),
+                                    ],
+                                  )
+                              ),
+                              vSpace,
+                            ],
+                          )
+                      ),
+                    ],
+                  )
               ),
-              Card(
-                child: ListTile(
-                  tileColor: theme.primaryColorLight,
-                  title: Text(
-                      'Types of Knots',
-                      style: theme.textTheme.headlineSmall
-                  ),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        subtitleText("Forward Knot"),
-                        Text("1. Create a '4' by placing your working string over the interior string.", style: theme.textTheme.bodyMedium),
-                        Text("2. Pull the end of the working string under and through the hole in the '4' and tighten.", style: theme.textTheme.bodyMedium),
-                        Text("3. Repeat once more.", style: theme.textTheme.bodyMedium),
-                        subtitleText("Backward Knot"),
-                        Text("Create a backwards '4' with your left and right strings. Pull the end of the right string under and through the hole in the '4' and tighten. Repeat once more.", style: theme.textTheme.bodyMedium),
-                        subtitleText("Forward-Backward Knot"),
-                        Text("Create a '4' with your left and right strings. Pull the end of the left string under and through the hole in the '4' and tighten. ", style: theme.textTheme.bodyMedium),
-                        subtitleText("Backward-Forward Knot"),
-                        Text("Create a '4' with your left and right strings. Pull the end of the left string under and through the hole in the '4'.", style: theme.textTheme.bodyMedium),
-                      ],
-                    )
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  title: Text(
-                      'How to Read Patterns',
-                      style: theme.textTheme.headlineSmall
-                  ),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text("1. hello"),
-                        Text("hi"),
-                      ],
-                    )
-                ),
+              Card (
+                  child: ExpansionTile(
+                    title: Text(
+                        'How to Read Patterns',
+                        style: theme.textTheme.headlineSmall
+                    ),
+                    children: <Widget>[
+                      ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("1. hello"),
+                              Text("hi"),
+                            ],
+                          )
+                      ),
+                    ],
+                  )
               ),
             ]
         )
